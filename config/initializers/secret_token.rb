2279376df888bc830eb33d96406a45535aee939a
lfborjas <luis.borjas@escolarea.com>
2010-11-16 09:48:42 -0600

    instalados jquery, devise, cucumber, formtastic y web_app_theme, además de borrar el public.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PasteIt::Application.config.secret_token = '62a712699aad9578facb19abec591ddb851e696cc0db7df3e9377aa6e744ab6fcf6599935c7a117c5b804737afa2ed4c84cb8520001026c9a2f397be8d131643'
