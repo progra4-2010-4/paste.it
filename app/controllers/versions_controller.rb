ec2fad6caddd79d2914cb8f5528b0a405a1ba8cd
lfborjas <luis.borjas@escolarea.com>
2010-11-18 14:33:25 -0600

    navegando entre versiones

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,8 +1,15 @@
 class VersionsController < ApplicationController
   def show
+    @snippet = Version.find(params[:id]).reify || Snippet.find(params[:snippet_id])
+    @sections = @snippet.get_sections
+    @versions = @snippet.versions
+    #original = Snippet.find params[:snippet_id]
+    #@diff = Differ.diff current, original
+    render 'snippets/show'
   end
 
   def compare
+    p params
   end
 
 end
