4211313625b29f7c934c1cfb488db4cf53f43fc5
lfborjas <luis.borjas@escolarea.com>
2010-11-18 02:09:53 -0600

    pasado a coderay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def highlight_code(language, content, lines=-1) 
+   CodeRay.scan(content.split("\n")[0..lines], language).div :line_numbers=>:table, :css => :class 
+  end
 end
