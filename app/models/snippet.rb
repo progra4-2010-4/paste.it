69f67271a18d5e373e6eb85b66e6ae94a04bc5d6
lfborjas <luis.borjas@escolarea.com>
2010-11-18 12:56:13 -0600

    con pruebas empíricas, ya está el seccionado

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,6 +1,39 @@
 class Snippet < ActiveRecord::Base
   belongs_to :user
-  LANGUAGES = CodeRay::Scanners.list.sort 
+  LANGUAGES = CodeRay::Scanners.list.sort
+  LANGUAGES_HASH = {"html"=>:html,
+                    "xml"=>:xml,
+                    "htm"=>:html,
+                    "mysql"=>:sql,
+                    "yml"=>:yaml,
+                    "php"=>:php,
+                    "c"=>:c, 
+                    "sql"=>:sql,
+                    "css"=>:css,
+                    "cpp"=>:cpp,
+                    "c++"=>:cpp,
+                    nil=>:plaintext,
+                    "nitro_xhtml"=>:nitro_xhtml,
+                    "java"=>:java,
+                    "groovy"=>:groovy,
+                    "txt"=>:plaintext,
+                    "rb"=>:ruby,
+                    "json"=>:json,
+                    "python"=>:python,
+                    "diff"=>:diff, 
+                    "scheme"=>:scheme,
+                    "yaml"=>:yaml,
+                    "debug"=>:debug,
+                    "delphi"=>:delphi,
+                    "h"=>:c,
+                    "ruby"=>:ruby,
+                    "hpp"=>:cpp,
+                    "js"=>:java_script, 
+                    "xsd"=>:xml,
+                    "py"=>:python,
+                    "rhtml"=>:rhtml,
+                    "erb"=>:rhtml}
+
   DEFAULT_LANGUAGE = "ruby" 
   before_save :set_language 
   
@@ -13,7 +46,7 @@ class Snippet < ActiveRecord::Base
   def get_sections 
    #the shebanged lines are always one less than the gruops
    sep =
-   shebanged_lines = self.content.scan(/^##!\s+(\w+).*\n/).flatten
+   shebanged_lines = self.content.scan(/^##!\s+([\w.]+).*\n/).flatten
    groups = self.content.split /^##!\s+\w+.*\n/
    default_hash =  [{:language=>self.language, :content=>self.content}]
    unless shebanged_lines.any? 
@@ -21,7 +54,13 @@ class Snippet < ActiveRecord::Base
    end
 
    begin
-       shebanged_lines.each_index.collect{|i| {:language=>shebanged_lines[i], :content=>groups[i+1]}}
+        
+       shebanged_lines.each_index.collect do |i|
+         title, ft= shebanged_lines[i].scan(/(\w+)\.?(\w+)?/).flatten
+         #swap if ft is nil
+         title, ft = ft, title if ft.nil?
+         {:language=>LANGUAGES_HASH[ft], :content=>groups[i+1], :title=>shebanged_lines[i]}
+       end
    rescue
      return default_hash
    end
