a083665bf23424d15c1fe8d77ac7352ab73e9af6
lfborjas <luis.borjas@escolarea.com>
2010-11-18 15:15:57 -0600

    diffing!

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,6 +1,8 @@
 class VersionsController < ApplicationController
   def show
-    @snippet = Version.find(params[:id]).reify || Snippet.find(params[:snippet_id])
+    v = Version.find(params[:id])
+    @snippet = v.reify
+    return redirect_to snippet_path(Snippet.find(params[:snippet_id])) if @snippet.nil?
     @sections = @snippet.get_sections
     @versions = @snippet.versions
     #original = Snippet.find params[:snippet_id]
@@ -9,7 +11,26 @@ class VersionsController < ApplicationController
   end
 
   def compare
-    p params
+    c, o = params[:versions]
+    @snippet = Snippet.find params[:snippet_id]
+    if Version.exists?(:id=>c) && Version.exists?(:id=>o) 
+      raw_current  = Version.find(c)
+      raw_original = Version.find(o)
+      #only allow valid comparisons
+      return redirect_to snippet_path(@snippet) if raw_current.item_id != @snippet.id || raw_original.item_id != @snippet.id
+
+      #do the actual comparison
+      current = raw_current.reify || @snippet
+      original = raw_original.reify || @snippet
+
+      @diff = Differ.diff(current.content, original.content).format_as :html
+      @current  = c
+      @original = o
+      @versions = @snippet.versions
+      render 'snippets/show'
+    else
+      redirect_to snippet_path @snippet
+    end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def highlight_code(language, content, lines=-1) 
    CodeRay.scan(content.split("\n")[0..lines], language).div :line_numbers=>:table, :css => :class 
   end
+
+  def diff_class(id) 
+    if @current && @original 
+      id == @current  ? "current"  : ""
+      id == @original ? "original" : ""
+    end
+    ""
+  end
 end
