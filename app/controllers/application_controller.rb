0146de444a595c3d3aadf598e59105a48b9c2163
lfborjas <luis.borjas@escolarea.com>
2010-11-17 23:30:50 -0600

    generado el controller de snippets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  private
+    def record_not_found 
+     redirect_to root_path 
+    end
 end
---
+++ app/controllers/snippets_controller.rb
@@ -0,0 +1,23 @@
+class SnippetsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def diff
+  end
+
+end
